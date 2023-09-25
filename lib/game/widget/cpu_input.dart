c7bf4e9e7862446f40d56989ed90bc156c26ff67
Yuhyeon0516 <rladbgus0516@naver.com>
2023-09-25 17:07:27 +0900

    If user input was entered, cpu result displayed.

--- lib/game/widget/cpu_input.dart
+++ lib/game/widget/cpu_input.dart
@@ -5,25 +5,33 @@ import 'package:rock_paper_scissors/game/widget/input_card.dart';
 class CpuInput extends StatelessWidget {
   final bool isDone;
   final InputType cpuInput;
+
   const CpuInput({required this.isDone, required this.cpuInput, super.key});
 
   @override
   Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Expanded(child: SizedBox.shrink()),
+        InputCard(
+          child: getCpuInput(),
+        ),
+        const Expanded(child: SizedBox.shrink())
+      ],
+    );
+  }
+
+  Widget getCpuInput() {
     if (isDone) {
-      return const Placeholder();
+      return Image.asset(cpuInput.path);
     }
 
-    return const Center(
-      child: InputCard(
-        child: SizedBox(
-          width: 60,
-          height: 60,
-          child: Center(
-            child: Text(
-              "?",
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-            ),
-          ),
+    return const SizedBox(
+      height: 80,
+      child: Center(
+        child: Text(
+          "?",
+          style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
         ),
       ),
     );
